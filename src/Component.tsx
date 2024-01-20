01c500178a560784a79a8c9fd59c66148ee8c48b
juliusmarminge <julius0216@outlook.com>
2024-01-20 23:34:33 +0100

    add comments

--- src/Component.tsx
+++ src/Component.tsx
@@ -1,5 +1,10 @@
 import { defineComponent, ref } from "vue";
 
+/**
+ * This component get's inferred as any
+ * Even though it's a copy-pasta from the docs:
+ * @see https://vuejs.org/api/general.html#function-signature
+ */
 export const GenericComp = defineComponent(
   <T extends string | number>(props: { msg: T; list: T[] }) => {
     // use Composition API here like in <script setup>
@@ -20,6 +25,9 @@ export const GenericComp = defineComponent(
   }
 );
 
+/**
+ * This component get's properly typed
+ */
 export const Comp = defineComponent(
   (props: { msg: string; list: string[] }) => {
     // use Composition API here like in <script setup>
